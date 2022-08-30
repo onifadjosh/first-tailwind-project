e1135e5dbad38144a6d38fb0fcbf273085a90845
onifadjosh <onifadjosh@gmail.com>
2022-08-30 08:14:57 +0100

    project complete!!!!!!

--- src/scripts.js
+++ src/scripts.js
@@ -36,4 +36,25 @@ function showSlides() {
 }
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+   let obj = document.getElementById(id),
+    current = start,
+    range = end - start,
+    increment = end > start ? 1 : -1,
+    step = Math.abs(Math.floor(duration / range)),
+    timer = setInterval(() => {
+     current += increment;
+     obj.textContent = current;
+     if (current == end) {
+      clearInterval(timer);
+     }
+    }, step);
+  }
+  counter("count1", 1000, 2000, 2000);
+  counter("count2", 100, 1500, 1500);
+  counter("count3", 200, 700, 700);
+  counter("count4", 100, 500, 500);
+ });
+
 
